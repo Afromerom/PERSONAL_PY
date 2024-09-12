fd49dadb3f626fc47c3d9d8a68658a82347ec42d
Andrés Felipe Romero Medina <158528488+Afromerom@users.noreply.github.com>
2024-09-11 20:23:54 -0500

    appversion1

--- LocalApp/mainapp.py
+++ LocalApp/mainapp.py
@@ -1,10 +1,15 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from PyQt5.QtWidgets import QFileDialog, QTableWidget, QTableWidgetItem
+import pandas as pd
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(913, 510)
+        # Inicializar variables para almacenar los datos cargados
+        self.mrna_data = None
+        self.mirna_data = None
+        
         self.Versionstate = QtWidgets.QLabel(Dialog)
         self.Versionstate.setGeometry(QtCore.QRect(370, 50, 211, 21))
         font = QtGui.QFont()
@@ -58,15 +63,22 @@ class Ui_Dialog(object):
         self.valor1_3.setFont(font)
         self.valor1_3.setText("")
         self.valor1_3.setObjectName("valor1_3")
+        
+        
         self.UPLOAD_MRNA = QtWidgets.QPushButton(Dialog)
         self.UPLOAD_MRNA.setGeometry(QtCore.QRect(380, 120, 101, 31))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.UPLOAD_MRNA.setFont(font)
         self.UPLOAD_MRNA.setObjectName("UPLOAD_MRNA")
-        self.GRAF_ACEL = QtWidgets.QWidget(Dialog)
-        self.GRAF_ACEL.setGeometry(QtCore.QRect(20, 230, 421, 161))
-        self.GRAF_ACEL.setObjectName("GRAF_ACEL")
+        self.UPLOAD_MRNA.setText("UPLOAD MRNA")
+        
+        self.GRAF_MRNA = QTableWidget(Dialog)
+        self.GRAF_MRNA.setGeometry(QtCore.QRect(20, 230, 421, 161))
+        self.GRAF_MRNA.setObjectName("GRAF_MRNA")
+        
+        
+        
         self.dataload1 = QtWidgets.QLabel(Dialog)
         self.dataload1.setGeometry(QtCore.QRect(130, 130, 231, 21))
         font = QtGui.QFont()
@@ -74,16 +86,23 @@ class Ui_Dialog(object):
         font.setPointSize(16)
         self.dataload1.setFont(font)
         self.dataload1.setObjectName("dataload1")
+        
+        
         self.progressBarmRNA = QtWidgets.QProgressBar(Dialog)
         self.progressBarmRNA.setGeometry(QtCore.QRect(500, 130, 118, 23))
-        self.progressBarmRNA.setProperty("value", 24)
+        self.progressBarmRNA.setProperty("value", 0)
         self.progressBarmRNA.setObjectName("progressBarmRNA")
+        
+        
         self.CHECKSTATE = QtWidgets.QPushButton(Dialog)
         self.CHECKSTATE.setGeometry(QtCore.QRect(720, 130, 101, 31))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.CHECKSTATE.setFont(font)
         self.CHECKSTATE.setObjectName("CHECKSTATE")
+        self.CHECKSTATE.setText("CHECK")
+        
+        
         self.dataload2 = QtWidgets.QLabel(Dialog)
         self.dataload2.setGeometry(QtCore.QRect(130, 160, 231, 21))
         font = QtGui.QFont()
@@ -91,19 +110,25 @@ class Ui_Dialog(object):
         font.setPointSize(16)
         self.dataload2.setFont(font)
         self.dataload2.setObjectName("dataload2")
+        
         self.UPLOAD_MIRNA = QtWidgets.QPushButton(Dialog)
         self.UPLOAD_MIRNA.setGeometry(QtCore.QRect(380, 160, 101, 31))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.UPLOAD_MIRNA.setFont(font)
         self.UPLOAD_MIRNA.setObjectName("UPLOAD_MIRNA")
+        self.UPLOAD_MIRNA.setText("UPLOAD miRNA")
+        
+        
         self.progressBarmiRNA = QtWidgets.QProgressBar(Dialog)
         self.progressBarmiRNA.setGeometry(QtCore.QRect(500, 160, 118, 23))
-        self.progressBarmiRNA.setProperty("value", 24)
+        self.progressBarmiRNA.setProperty("value", 0)
         self.progressBarmiRNA.setObjectName("progressBarmiRNA")
-        self.GRAF_ACEL_2 = QtWidgets.QWidget(Dialog)
-        self.GRAF_ACEL_2.setGeometry(QtCore.QRect(470, 230, 421, 161))
-        self.GRAF_ACEL_2.setObjectName("GRAF_ACEL_2")
+        
+        self.GRAF_MIRNA = QTableWidget(Dialog)
+        self.GRAF_MIRNA.setGeometry(QtCore.QRect(470, 230, 421, 161))
+        self.GRAF_MIRNA.setObjectName("GRAF_MIRNA")
+        
         self.CONFIRMATION1 = QtWidgets.QLabel(Dialog)
         self.CONFIRMATION1.setGeometry(QtCore.QRect(630, 130, 71, 21))
         font = QtGui.QFont()
@@ -181,6 +206,79 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
+        
+         # Conectar los botones a sus funciones correspondientes
+        self.UPLOAD_MRNA.clicked.connect(self.upload_mrna)
+        self.UPLOAD_MIRNA.clicked.connect(self.upload_mirna)
+        self.CHECKSTATE.clicked.connect(self.show_csv_data) 
+        
+        
+    def upload_mrna(self):
+        fileName, _ = QFileDialog.getOpenFileName(
+            None, 
+            "Select CSV File", 
+            "", 
+            "CSV Files (*.csv);;All Files (*)"
+        )
+        if fileName:
+            print("mRNA file selected:", fileName)
+            self.progressBarmRNA.setValue(0)
+            self.mrna_data = pd.read_csv(fileName)
+            self.simulate_progress(self.progressBarmRNA, self.CONFIRMATION1)
+
+    def upload_mirna(self):
+        fileName, _ = QFileDialog.getOpenFileName(
+            None, 
+            "Select CSV File", 
+            "", 
+            "CSV Files (*.csv);;All Files (*)"
+        )
+        if fileName:
+            print("miRNA file selected:", fileName)
+            self.progressBarmiRNA.setValue(0)
+            self.mirna_data = pd.read_csv(fileName)
+            self.simulate_progress(self.progressBarmiRNA, self.CONFIRMATION2)
+
+    def simulate_progress(self, progressBar, confirmationLabel):
+        # Simular la carga del archivo con un temporizador
+        self.timer = QtCore.QTimer()
+        self.progress_value = 0
+        self.timer.timeout.connect(lambda: self.update_progress(progressBar, confirmationLabel))
+        self.timer.start(100)  # Simular la carga cada 100ms
+
+    def update_progress(self, progressBar, confirmationLabel):
+        if self.progress_value < 100:
+            self.progress_value += 10
+            progressBar.setValue(self.progress_value)
+        else:
+            self.timer.stop()
+            confirmationLabel.setText("loaded")
+            confirmationLabel.setStyleSheet("color: green")
+
+    def show_csv_data(self):
+        # Mostrar las primeras 5 filas y 4 columnas del archivo mRNA
+        if self.mrna_data is not None:
+            mrna_preview = self.mrna_data.iloc[:5, :4]
+            self.populate_table(self.GRAF_MRNA, mrna_preview)
+        
+        # Mostrar las primeras 5 filas y 4 columnas del archivo miRNA
+        if self.mirna_data is not None:
+            mirna_preview = self.mirna_data.iloc[:5, :4]
+            self.populate_table(self.GRAF_MIRNA, mirna_preview)
+
+    def populate_table(self, table_widget, data):
+        # Configurar el número de filas y columnas del widget
+        table_widget.setRowCount(data.shape[0])
+        table_widget.setColumnCount(data.shape[1])
+        
+        # Insertar los encabezados de columna
+        table_widget.setHorizontalHeaderLabels(data.columns)
+
+        # Insertar los datos en la tabla
+        for i in range(data.shape[0]):
+            for j in range(data.shape[1]):
+                table_widget.setItem(i, j, QTableWidgetItem(str(data.iat[i, j])))
+
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
